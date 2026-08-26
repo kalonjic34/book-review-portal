abf4a57e25ffc72d27276c9eb36fa5385d2a45f9
Christian <kalonjic34@gmail.com>
2026-08-26 16:21:32 +0200

    refactor BookFactory to remove placeholder and clarify default state definition

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -18,7 +18,10 @@ class BookFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title'=>fake()->sentence(3),
+            'author'=>fake()->name(),
+            'created_at'=>fake()->dateTimeBetween('-2 years'),
+            'updated_at'=>fake()->dateTimeBetween('created_at','now'),
         ];
     }
 }
