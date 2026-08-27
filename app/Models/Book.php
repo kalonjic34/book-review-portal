7568a6c54f079355e82b2e76d80538f70e1eb877
Christian <kalonjic34@gmail.com>
2026-08-27 13:31:20 +0200

    refactor Book model to include fillable properties and scope for title search

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['review', 'rating'];
+
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder{
+        return $query->where('title','LIKE','%'. $title. '%');
     }
 }
