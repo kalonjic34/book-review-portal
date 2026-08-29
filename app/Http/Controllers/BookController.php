857c406534e800421001d70ae44648849fa87172
Christian <kalonjic34@gmail.com>
2026-08-29 17:29:17 +0200

    add filtering options to book listing in index view

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,10 +13,20 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
         $books = Book::when($title, function($query, $title){
             return $query->title($title);
-        })
-        ->get();
+        });
+
+        $books= match($filter){
+            'popular_last_month'=>$books->popularLastMonth(),
+            'popular_last_6months'=>$books->popularLast6Month(),
+            'highest_rated_last_month'=>$books->highestRatedLastMonth(),
+            'highest_rated_last_6months'=>$books->highestRatedLast6Months(),
+
+            default => $books->latest()
+        };
+        $books = $books->get();
 
         return view('books.index', ['books'=> $books]);
     }
