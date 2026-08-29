d8b09cf11fa19ace098f50718bc28d94574b526b
Christian <kalonjic34@gmail.com>
2026-08-29 17:31:21 +0200

    refactor Book model to enhance review filtering and add new scopes for popular and highest rated books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -21,17 +21,21 @@ class Book extends Model
     }
 
     public function scopePopular(Builder $query, $from = null, $to = null):Builder{
-        return $query ->withCount([
-            'reviews'=> fn(Builder $q) => $this-> dateRangeFilter($q,$from, $to)
-            ])
+        return $query->withCount([
+            'reviews'=> fn(Builder $q) => $this->dateRangeFilter($q,$from, $to)
+        ])->withAvg([
+            'reviews'=> fn(Builder $q) => $this->dateRangeFilter($q,$from, $to)
+        ],'rating')
         ->orderBy('reviews_count','desc');
     }
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withAvg([
-            'reviews'=> fn(Builder $q) => $this-> dateRangeFilter($q,$from, $to)
-            ],'rating')
+        return $query->withCount([
+            'reviews'=> fn(Builder $q) => $this->dateRangeFilter($q,$from, $to)
+        ])->withAvg([
+            'reviews'=> fn(Builder $q) => $this->dateRangeFilter($q,$from, $to)
+        ],'rating')
         ->orderBy('reviews_avg_rating','desc');
     }
 
@@ -39,15 +43,31 @@ class Book extends Model
         return $query->having('reviews_count','>=',$minReviews);
     }
 
-    private function dateRangeFilter(Builder $query, $from = null, $to = null){
-
-                if ($from && !$to) {
-                    $query->where('created_at','>=',$from);
-                }elseif(!$from && $to){
-                    $query->where('created_at','<=',$to);
-                }elseif($from && $to){
-                    $query->whereBetween('created_at',[$from, $to]);
-                }
-            }
+    private function dateRangeFilter(Builder $query, $from = null, $to = null): Builder {
+        if ($from && !$to) {
+            $query->where('created_at','>=',$from);
+        } elseif (!$from && $to) {
+            $query->where('created_at','<=',$to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at',[$from, $to]);
+        }
+        return $query;
+    }
+    
+    public function scopePopularLastMonth(Builder $query): Builder {
+        return $query->popular(now()->subMonth(), now())->minReviews(2);
+    }
+    
+    public function scopePopularLast6Month(Builder $query): Builder {
+        return $query->popular(now()->subMonths(6), now())->minReviews(5);
+    }
+    
+    public function scopeHighestRatedLastMonth(Builder $query): Builder {
+        return $query->highestRated(now()->subMonth(), now())->minReviews(2);
+    }
+    
+    public function scopeHighestRatedLast6Months(Builder $query): Builder {
+        return $query->highestRated(now()->subMonths(6), now())->minReviews(5);
+    }
     }
 
