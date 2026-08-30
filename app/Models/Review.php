788bacf5eb92debd83c11dba0a9907222d7203f6
Christian <kalonjic34@gmail.com>
2026-08-30 18:07:02 +0200

    clear book cache on review events

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
 
 class Review extends Model
 {
@@ -11,5 +12,11 @@ class Review extends Model
 
     public function book(){
         return $this->belongsTo(Book::class);
+    }
+
+    protected static function booted()
+    {
+        static::updated(fn (Review $review)=> cache()->forget('book:'.$review->book_id));
+        static::deleted(fn (Review $review)=> cache()->forget('book:'.$review->book_id));
     }
 }
